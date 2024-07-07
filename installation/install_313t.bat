293fbcf2392da826cf3d60fb9dad826270c7d513
K4liber <janbielecki94@gmail.com>
2024-07-07 10:36:10 +0200

    continue

--- installation/install_313t.bat
+++
@@ -1,4 +0,0 @@
-set PYTHON313t_PATH="C:\Users\kamka\AppData\Local\Programs\Python\Python313\python3.13t.exe"
-call %PYTHON313t_PATH% -m venv .venv313t
-call .venv313t\Scripts\activate
-@rem " comment out psutils library usage in runner/interface"
